a0df85cb2165202bd9d0fe7ff718e8434f148909
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-20 23:06:15 -0600

    Not finished

---
+++ index.js
@@ -0,0 +1,18 @@
+// ax = ["John", "Paul", "George", "Pete"]
+// console.log(ax[3])
+
+// ax = ["John", "Paul", "George", "Pete"]
+// console.log(ax[3] = 'Ringo')
+
+//* ax = ["John", "Paul", "George", "Pete"]
+// ax = unshift('Yoko')
+// console.log(ax)
+
+
+
+
+Ages = prompt("Enter four ages")
+ax = [Ages]
+
+console.log(ax)
+
